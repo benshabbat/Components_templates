31c021c25647ed8381497d5932ecf9450d9e3839
benshabbat <benshabbat27@gmail.com>
2023-07-02 23:18:59 +0300

    starting register form added userefs err and user and ustates for validation

--- form_validation/src/components/Register.jsx
+++ form_validation/src/components/Register.jsx
@@ -2,6 +2,12 @@ import {useState,useRef,useEffect} from 'react'
 import  {faCheck,faTime,faInfoCircle} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Register = () => {
+  const userRef = useRef()
+  const errRef = useRef()
+
+  const [user, setUser] = useState("")
+  const [validName, setValidName] = useState(false)
+  const [userFocus, setUserFocus] = useState(false)
   return (
     <div>
       
