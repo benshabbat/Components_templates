e014743921b876c232fc416e90cde7b21b0fc9d7
benshabbat <benshabbat27@gmail.com>
2023-07-02 21:29:34 +0300

    fortawesome install

---
+++ form_validation/src/components/Register.jsx
@@ -0,0 +1,12 @@
+import {useState,useRef,useEffect} from 'react'
+import  {faCheck,faTime,faInfoCircle} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const Register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Register
